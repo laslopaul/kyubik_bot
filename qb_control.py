4b3f9ff0dc607cb63d3988118cd3f02219871744
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-03-23 23:31:56 +0300

    Minor code improvements

--- qb_control.py
+++ qb_control.py
@@ -2,29 +2,26 @@
 # Implements commands of Qbittorrent WebAPI that are necessary for the bot
 # https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)
 
+
+from ctypes import Union
 import json
 import os.path
 import requests
-
-
-class LoginError(requests.exceptions.RequestException):
-    """Exception raised upon unsuccessful Qbittorrent login"""
 
 
 class QBWebAPI:
     """Class that implements main Qbittorrent WebAPI commands"""
 
     def __init__(self, config_path: str) -> None:
-        if os.path.exists(config_path):
-            with open(config_path) as f:
-                conf = json.loads(f.read())
+        if not os.path.exists(config_path):
+            raise OSError("Configuration file not found")
 
-            self.server = conf['Server']
-            self._login(conf['Username'], conf['Password'])
-            self._get_hashdict()
-
-        else:
-            raise OSError('Configuration file not found')
+        with open(config_path) as f:
+            conf = json.load(f)
+        self.server: str = conf["Server"]
+        self.base_url = self.server + "/api/v2/{}/{}"
+        self._login(conf["Username"], conf["Password"])
+        self._get_hashdict()
 
     def __del__(self) -> None:
         """Performs logout before deleting QBWebAPI instance"""
@@ -33,31 +30,32 @@ class QBWebAPI:
 
     def _login(self, username: str, password: str) -> None:
         """Performs login to the server"""
-        api_method = self.server + '/api/v2/auth/login'
-        header_dict = {'Referer': self.server}
-        auth_dict = {'username': username, 'password': password}
+        api_method = self.base_url.format("auth", "login")
+        header_dict = {"Referer": self.server}
+        auth_dict = {"username": username, "password": password}
 
         r = requests.post(api_method, headers=header_dict, data=auth_dict)
         if not r.ok:
-            raise requests.exceptions.HTTPError(
-                'Your IP is banned for too many failed login attempts'
+            raise requests.exceptions.ConnectionError(
+                "Your IP is banned for too many failed login attempts"
             )
-        elif r.ok and r.text == 'Fails.':
-            raise LoginError('Incorrect username or password')
-        elif r.ok and r.text == 'Ok.':
+        elif r.ok and r.text == "Fails.":
+            raise requests.exceptions.ConnectionError(
+                "Incorrect username or password"
+            )
+        elif r.ok and r.text == "Ok.":
             # Login successful, getting auth cookie
-            sid = r.headers['set-cookie'].split(';')[0]
+            sid = r.headers["set-cookie"].split(";")[0]
             self._auth_cookie = {sid[:3]: sid[4:]}
 
     def _logout(self) -> None:
-        api_method = self.server + '/api/v2/auth/logout'
+        api_method = self.base_url.format("auth", "logout")
         requests.get(api_method, cookies=self._auth_cookie)
 
     def _get_default_save_path(self) -> str:
         """Get default path for saving downloads"""
-        api_method = self.server + '/api/v2/app/defaultSavePath'
-        r = requests.get(api_method, cookies=self._auth_cookie)
-        return r.text
+        api_method = self.base_url.format("app", "defaultSavePath")
+        return requests.get(api_method, cookies=self._auth_cookie).text
 
     def _get_hashdict(self) -> None:
         """
@@ -65,16 +63,15 @@ class QBWebAPI:
         for search purposes
 
         """
-        api_method = self.server + '/api/v2/torrents/info'
+        api_method = self.base_url.format("torrents", "info")
         # Getting info about all torrents on the server
-        r = requests.get(api_method, cookies=self._auth_cookie)
-        torrents = r.json()
+        torrents = requests.get(api_method, cookies=self._auth_cookie).json()
         # Adding torrents names and hashes to a dictionary
         self.hashdict = {
-            torrent['name']: torrent['hash'] for torrent in torrents
+            torrent["name"]: torrent["hash"] for torrent in torrents
         }
 
-    def _search_hash(self, torrent_name: str):
+    def _search_hash(self, torrent_name: str) -> Union[str, None]:
         """Searches a torrent by its name and returns its hash"""
         i = 0
         torrent_names = list(self.hashdict.keys())
